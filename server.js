dbbd08f4d75a0af1edc94a50001dec361b49807c
wanwan510 <kanwunwan@gmail.com>
2025-11-26 16:07:11 +0800

    261125

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
 
+
+server.set("view engine", 'ejs');
+server.set("views", __dirname+"/view")
+
+var fileUpload = require("express-fileupload");
+
 server.use(express.static(__dirname + "/Public"));
 server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+server.use(fileUpload({limits:{fileSize:2*1024*1024}}))
 
 // database setup eg.
 var DB=require("nedb-promises");
 var ServiceDB = DB.create(__dirname+"/Service.db");
 var PortfolioDB = DB.create(__dirname+"/Portfolio.db");
+var ContactDB = DB.create(__dirname+"/Contact.db");
 
 //add data once
 // ServiceDB.insert([
@@ -72,7 +81,18 @@ server.get("/about", (req, res) => {
 })
 
 server.post("/contact", (req, res) => {
-
+    ContactDB.insert(req.body);
+    //move to public/upload
+    var upFile=req.files.myFile1;
+    upFile.mv(__dirname+"/public/upload/"+upFile.name, function(err){
+        if(err==null){
+            res.render("msg",{message:" I got a file: "+upFile.name})
+            //res.redirect("/about.html")
+        }else{
+            res.render("msg",{message:err});
+            //res.remder("/error.html",err);
+        }
+    })
 })
     
 server.listen(80)
